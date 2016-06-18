c1e62c902591a40f480f61a3fcf6113c52528e33
Joseph Quigley <cpu.crazy@gmail.com>
2016-06-18 14:23:19 -0500

    Modified setup convenience method to allow for attaching to view controllers that have detached AutoCompleteDelegates

--- Autocomplete/AutoComplete/Autocomplete.swift
+++ Autocomplete/AutoComplete/Autocomplete.swift
@@ -9,21 +9,38 @@
 import UIKit
 public class Autocomplete {
     public class func setupAutocompleteForViewcontroller<T: UIViewController where T: AutocompleteDelegate>(viewController: T) {
-
-        let podBundle: NSBundle = NSBundle(forClass: Autocomplete.self)
-
-        let storyboard = UIStoryboard(name: "Autocomplete", bundle: podBundle)
-        let autoCompleteViewController = storyboard.instantiateViewControllerWithIdentifier("autocompleteScene") as! AutoCompleteViewController
-        
+        let autoCompleteViewController = initAutoCompleteVC()
         autoCompleteViewController.delegate = viewController
 
-        autoCompleteViewController.willMoveToParentViewController(viewController)
-        viewController.addChildViewController(autoCompleteViewController)
-        autoCompleteViewController.didMoveToParentViewController(viewController)
+        setupUI(autoCompleteViewController, parentViewController: viewController)
 
-        autoCompleteViewController.view.willMoveToSuperview(viewController.view)
-        viewController.view.addSubview(autoCompleteViewController.view)
+    }
+    public class func setupAutocompleteForViewcontrollerWithDetachedDelegate(viewController: UIViewController, delegate:AutocompleteDelegate) {
+        let autoCompleteViewController = initAutoCompleteVC()
+        autoCompleteViewController.delegate = delegate
+        
+        setupUI(autoCompleteViewController, parentViewController: viewController)
+        
+    }
+    
+    private class func initAutoCompleteVC() -> AutoCompleteViewController {
+        let podBundle: NSBundle = NSBundle(forClass: Autocomplete.self)
+        
+        let storyboard = UIStoryboard(name: "Autocomplete", bundle: podBundle)
+        return storyboard.instantiateViewControllerWithIdentifier("autocompleteScene") as! AutoCompleteViewController
+    }
+    
+    private class func setupUI(autoCompleteViewController: AutoCompleteViewController, parentViewController: UIViewController) {
+        //Remove from any superview and super viewcontrollers
+        autoCompleteViewController.view.removeFromSuperview()
+        autoCompleteViewController.removeFromParentViewController()
+        
+        autoCompleteViewController.willMoveToParentViewController(parentViewController)
+        parentViewController.addChildViewController(autoCompleteViewController)
+        autoCompleteViewController.didMoveToParentViewController(parentViewController)
+        
+        autoCompleteViewController.view.willMoveToSuperview(parentViewController.view)
+        parentViewController.view.addSubview(autoCompleteViewController.view)
         autoCompleteViewController.view.didMoveToSuperview()
-
     }
 }
